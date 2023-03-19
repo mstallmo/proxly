f5c841257598b6c49b61a049a9d64912c311a99d
Mason Stallmo <masonstallmo@gmail.com>
2023-03-18 21:01:20 -0700

    Include tag helper with action view on load

--- lib/proxly/railtie.rb
+++ lib/proxly/railtie.rb
@@ -1,4 +1,7 @@
 module Proxly
   class Railtie < ::Rails::Railtie
+    initializer "proxly.tag_helper" do
+      ActiveSupport.on_load(:action_view) { include Proxly::TagHelper }
+    end
   end
 end
