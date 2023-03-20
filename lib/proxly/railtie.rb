dc3f4d0d48679fea8cf0b1e46d21ec63dc1df67f
Mason Stallmo <masonstallmo@hey.com>
2023-03-20 08:43:54 -0700

    Host router (#4)
    * Use asset_host for tunnel routing
    
    Setup a `before_action` to inspect the incoming request and change the
    host accordingly. All local requests will continue as normal and any
    remote requests will have the `asset_host` updated accorindgly.
    
    * Bump version for revisions
    
    * Update the railtie initializer name

--- lib/proxly/railtie.rb
+++ lib/proxly/railtie.rb
@@ -1,7 +1,26 @@
+# require_relative "./tunnels/cloudflare"
+
+# TODO:
+# - Try to implement auto-start stop in the gem
+
 module Proxly
   class Railtie < ::Rails::Railtie
-    initializer "proxly.tag_helper" do
-      ActiveSupport.on_load(:action_view) { include Proxly::TagHelper }
+    initializer "proxly.host_router", after: :load_config_initializers do
+      unless Rails.env.production?
+        ActiveSupport.on_load(:action_controller_base) do
+          include Proxly::HostRouter
+        end
+
+        # at_exit do
+        #   begin
+        #     Tunnels::Cloudflare.stop
+        #   rescue => e
+        #     puts "There was an #{e} while stopping cloudflare tunnel..."
+        #   ensure
+        #     puts "Tunnel shutdown"
+        #   end
+        # end
+      end
     end
   end
 end
---
+++ spec/proxly/host_router_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+module Proxly
+  RSpec.describe HostRouter do
+    it "placeholder" do
+    end
+  end
+end
